bc8b11a2f7374fa4aeb86d3ea04d77a5cbf2c2c1
yasin babur <119218777+yasiinb@users.noreply.github.com>
2025-04-23 20:27:10 +0300

    Add files via upload

---
+++ DernekOtomasyonu.Entities/Concrete/Odemeler.cs
@@ -0,0 +1,20 @@
+﻿using DernekOtomasyonu.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DernekOtomasyonu.Entities.Concrete
+{
+   public class Odemeler:IEntity
+    {
+        public int OdemeID { get; set; }
+        public int AidatID { get; set; }
+        public string UyeTC { get; set; }
+        public bool Durum { get; set; }     
+        public Uye Uye { get; set; }
+        public Aidat Aidat { get; set; }
+        
+    }
+}
---
+++ DernekOtomasyonu.Entities/Concrete/Uye.cs
@@ -0,0 +1,27 @@
+﻿using DernekOtomasyonu.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DernekOtomasyonu.Entities.Concrete
+{
+    public class Uye:IEntity
+    {
+ 
+        public string UyeTC { get; set; }
+        public string UyeAdi { get; set; }
+        public string UyeSoyadi { get; set; }
+        public string Kangurubu { get; set; }
+        public string UyeTelefonNo { get; set; }
+        public string Sehir { get; set; }
+        public string UyeMail { get; set; }
+        public DateTime KatilimTarihi { get; set; }
+        public int KalanAidat { get; set; }
+        public bool AktifDurum { get; set; }
+
+        public IEnumerable<Odemeler> odemelers { get; set; }
+
+    }
+}
