0917b7c2dedefa0327262ebde150e7da410c826f
yasin babur <119218777+yasiinb@users.noreply.github.com>
2025-04-23 20:25:09 +0300

    Add files via upload

---
+++ DernekOtomasyonu.Entities/Abstract/Concrete/Aidat.cs
@@ -0,0 +1,18 @@
+﻿using DernekOtomasyonu.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DernekOtomasyonu.Entities.Concrete
+{
+   public class Aidat:IEntity
+    {
+        public int AidatID { get; set; }
+        public int AidatMiktar { get; set; }
+        public DateTime AidatTarih { get; set; }
+        public IEnumerable<Odemeler> odemelers { get; set; }//Aidat ile Odemeler arasında bağlantı kurduk
+
+    }
+}
